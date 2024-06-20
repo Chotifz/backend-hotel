e45d9e5a7c2add7ad4b0b06dfcac3f35b5c5a85a
Marselllniiboyy <marseljayawijaya@gmail.com>
2024-06-20 12:42:16 +0800

    fix auth

--- src/routes/userLogInRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { test, login } = require("../controller/userLoginController");
-const router = express.Router();
-
-router.get("/", test);
-router.post("/", login);
-
-module.exports = router;
