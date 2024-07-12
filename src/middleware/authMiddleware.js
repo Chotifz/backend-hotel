9f0abbc596f831362e893fcc7a370f17b6e3322d
Chotifz <chotife@gmail.com>
2024-07-12 19:41:24 +0700

    add

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -18,4 +18,23 @@ const authenticate = async (req, res, next) => {
   });
 };
 
-module.exports = { authenticate };
+// const authenticateToken = (req, res, next) => {
+//   const token = req.header("Authorization").replace("Bearer ", "");
+
+//   if (!token) {
+//     return res.status(401).json({ message: "Access denied, token missing" });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: "Invalid token" });
+//   }
+// };
+
+module.exports = {
+  authenticate,
+  // authenticateToken
+};
